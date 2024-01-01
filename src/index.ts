6ef96e089c7d6d2c9cf414f50a5270d84f3f8e62
Kyle Close <k.james.close@gmail.com>
2024-01-01 14:39:15 -0800

    save

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,11 @@ import express, { Application } from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import router from './routes';
+import serviceAccount from '../bitelog-firebase.json';
 
-admin.initializeApp();
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+});
 
 const app: Application = express();
 const port = process.env.PORT || 8000;
