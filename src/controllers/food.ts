d6a56c01012a359e9ba4f540179c04ee3263bbe3
Kyle-Close <k.james.close@gmail.com>
2024-01-12 08:49:40 -0800

    save

--- src/controllers/food.ts
+++ src/controllers/food.ts
@@ -1,9 +1,12 @@
 import { body, validationResult } from 'express-validator';
 import asyncHandler from 'express-async-handler';
 import { NextFunction, Request, Response } from 'express';
+import Users from '../models/user';
+import Ingredient from '../models/ingredient';
 
 export const createUserFood = [
-  body('ingredientIds').isNumeric(),
+  body('ingredientIds').isArray().isLength({ min: 1 }),
+  body('ingredientIds.*').isNumeric(),
 
   asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
@@ -12,6 +15,43 @@ export const createUserFood = [
       return;
     }
 
+    // Check if all ingredients in list exists in user ingredient table
+    const userWithIngredients: any = await Users.findByPk(res.locals.uid, {
+      include: [
+        {
+          model: Ingredient,
+          attributes: ['id'], // Only fetch the ingredient IDs
+          through: {
+            attributes: [], // No need to fetch attributes from the join table
+          },
+        },
+      ],
+    });
+
+    console.dir(userWithIngredients);
+
+    if (!userWithIngredients) {
+      res.status(400).json({ err: 'Error fetching user ingredients.' });
+      return;
+    }
+
+    // .dataValues.id
+    // Extract ingredient IDs
+    const ingredientIds = userWithIngredients.Ingredients.map(
+      (data: any) => data.dataValues.id
+    );
+    const isValid = ingredientIds.every((id: string) =>
+      req.body.ingredientIds.includes(id)
+    );
+
+    console.log(isValid);
+    if (isValid) {
+      res.status(200).json({ msg: 'All valid' });
+    } else {
+      res
+        .status(400)
+        .json({ err: 'Not all ids exist in user ingredient table' });
+    }
     return;
   }),
 ];
